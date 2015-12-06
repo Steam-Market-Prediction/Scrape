9532678936d1d331a037bb69f9a6e319c63bc957
Justin <dengitzjg@gmail.com>
2015-12-06 01:11:29 -0500

    Added more filters to item_schema_parse

--- source/item_schema_parse.py
+++ source/item_schema_parse.py
@@ -6,7 +6,9 @@ num_items = 0
 for item_id in item_schema['items_game']['items']:
 	if "default" not in item_id:
 		if "default_item" not in item_schema['items_game']['items'][item_id]['prefab']:
-			num_items+=1
-			with open("items.txt", "a") as myfile:
-				myfile.write(item_schema['items_game']['items'][item_id]['name'] + '\n')
+			if "league" not in item_schema['items_game']['items'][item_id]['prefab']:
+				if "retired_treasure_chest" not in item_schema['items_game']['items'][item_id]['prefab']:
+					num_items+=1
+					with open("items.txt", "a") as myfile:
+						myfile.write(item_schema['items_game']['items'][item_id]['name'] + '\n')
 print "Parsed", num_items, "items"
